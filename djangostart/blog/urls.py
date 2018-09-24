aa3bdb6800fddc983e7ab6628410d6d8ee780822
woodpeck <woodpeck1@gmail.com>
2018-09-24 22:11:52 +1000

    add new function at blog app - insert and edit

--- djangostart/blog/urls.py
+++ djangostart/blog/urls.py
@@ -2,12 +2,16 @@ from django.urls import path
 
 from .views import (
     post_list_view,
-    post_detail_view
+    post_detail_view,
+    post_new_view,
+    post_edit_view
 )
 
 urlpatterns = [
     path('',post_list_view,name='post_list'),
     path('post/<int:id>',post_detail_view,name='post_detail'),
+    path('post/new',post_new_view,name='post_new'),
+    path('post/<int:id>/edit', post_edit_view, name='post_edit')
 
 ]
 
--- djangostart/blog/views.py
+++ djangostart/blog/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404, redirect
 from django.utils import timezone
 # Create your views here.
 # woodpeck -add
 
 from .models import Post
+from .forms import PostForm
 
 def post_list_view(request):
     post_all = Post.objects.all()
@@ -16,3 +17,41 @@ def post_detail_view(request,*args,**kwargs):
     #find_row = Post.objects.get(id=kwargs.get('id'))
     find_row = get_object_or_404(Post,id=kwargs.get('id'))
     return render(request,'post_detail.html',{'post':find_row})
+
+def post_new_view(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            #print(request.user)
+            #form.save()
+            post = form.save(commit=False)
+
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail',post.id)
+
+    else:
+        form = PostForm()
+    return render(request,'post_new.html',{'form':form})
+
+def post_edit_view(request,*args,**kwargs):
+    #find_row = Post.objects.get(id=kwargs.get('id'))
+    find_row = get_object_or_404(Post,id=kwargs.get('id'))
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=find_row)
+        if form.is_valid():
+            # print(request.user)
+            # form.save()
+            post = form.save(commit=False)
+
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', post.id)
+
+    else:
+        form = PostForm(instance=find_row)
+
+    return render(request,'post_edit.html',{'form':form})
